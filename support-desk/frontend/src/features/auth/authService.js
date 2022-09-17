4345cf0149aa05c0e1daa6116c580cb813510bca
Антон <akrikoff@gmail.com>
2022-09-17 14:46:33 +0300

    add authorization frontend

---
+++ support-desk/frontend/src/features/auth/authService.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const API_URL = "/api/users";
+const API_URL_LOGIN = "/api/users/login";
+
+// Register user
+const register = async (userData) => {
+  console.log("userData = ", userData);
+
+  const response = await axios.post(API_URL, userData);
+  console.log(response.data);
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+// Login user
+const login = async (userData) => {
+  console.log("userData = ", userData);
+
+  const response = await axios.post(API_URL_LOGIN, userData);
+  console.log(response.data);
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+// logout user
+const logout = () => localStorage.removeItem("user");
+
+const authService = {
+  register,
+  logout,
+  login,
+};
+
+export default authService;
